5d36a3910e25c093346f8171025619c6b0fb2c9a
sofialundstrom <117373294+sofialundstrom@users.noreply.github.com>
2022-11-25 08:34:11 +0100

    Add files via upload

---
+++ html/main.php
@@ -0,0 +1,12 @@
+<?php include('../txt/header.txt'); ?>
+    <h1>LEGO BANK</h1>
+    <p>Hej</p>
+    
+    <div id="overlay-search">
+            <form action="" method="GET">
+                <input type="text" name="searchbar" id="searchbar" placeholder="Search your lego piece">
+                <button type="submit">?</button>
+            </form>
+    </div>
+</body>
+</html>
