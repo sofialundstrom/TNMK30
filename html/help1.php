48a557c31a223bdbe04b49b9d75d7543f99a18fa
Hannisssss <Chat3y@gmail.com>
2022-11-25 10:04:14 +0100

    s

--- html/help1.php
+++ html/help1.php
@@ -1,4 +1,30 @@
 <?php include('../txt/header.txt'); ?>
 <h1> HOW TO SEARCH</h1>
+<div class="ruta1">
+    <div>
+        <img src="" alt="bild">
+    </div>
+    <div>
+        <ol>
+            <li> hej</li>
+            <li> hej</li>
+            <li> hej</li>
+            <li> hej</li>
+        </ol>
+    </div>
+</div>
+<div class="ruta2">
+    <div>
+        <img src="" alt="bild">
+    </div>
+    <div>
+        <ol>
+            <li> hej</li>
+            <li> hej</li>
+            <li> hej</li>
+            <li> hej</li>
+        </ol>
+    </div>
+</div>
 
 <?php include('../txt/footer.txt'); ?>
