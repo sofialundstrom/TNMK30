390800cd2cffb6908a7ee6e2aa257c0539689c03
sofialundstrom <soflu921@student.liu.se>
2022-12-01 11:39:42 +0100

    ij

--- html/searchpage.php
+++ html/searchpage.php
@@ -1,6 +1,8 @@
 <?php include('../txt/header.txt'); ?>
     <div class="searchContainer">
-        <input class="searchBox" type="search" name="search" placeholder="Search...">
+        <form action="searchpage.php" method="POST">
+            <input class="searchBox" type="search" name="search" placeholder="Search...">
+        </form>
     </div>
 
 <?php
@@ -8,20 +10,18 @@ $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
 if(!$connection){
     die('MySQL connection error');
 }
-$contents = mysqli_query ($connection, "SELECT parts.Partname, parts.PartID FROM parts WHERE Partname LIKE '%".$searchWord."%' OR PartID LIKE '%".$searchWord."%' ORDER BY LENGTH(Partname) ASC, PartID ASC LIMIT 5");
+$contents = mysqli_query ($connection, "SELECT parts.Partname, parts.PartID FROM parts WHERE Partname LIKE '%".$search."%' OR PartID LIKE '%".$search."%' ORDER BY LENGTH(Partname) ASC, PartID ASC LIMIT 5");
 
+print "<div class='container'>";
 while($row = mysqli_fetch_array($contents)) {
 
     $parts = $row['PartID'];
     $partname = $row['Partname'];
-    
-     print "<div class='container'>";
-    
-        print("<div class='part'>$parts$partname</div>");
+    print("<div class='part'>$parts$partname</div>");
         
-    print "</div>";
 }
+print "</div>";
 mysqli_close($connection);
 
-<?php include('../txt/footer.txt'); ?>
+include('../txt/footer.txt'); ?>
  
