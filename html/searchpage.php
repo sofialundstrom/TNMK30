f3a9c837eb548a8db5d67ddb4301fbcb7a9fa4d6
sofialundstrom <soflu921@student.liu.se>
2022-12-01 09:47:35 +0100

    nytt dokument & fixa sök i databas

---
+++ html/searchpage.php
@@ -0,0 +1,7 @@
+<?php include('../txt/header.txt'); ?>
+     <div class="searchContainer">
+    
+        <input class="searchBox" type="search" name="search" placeholder="Search...">
+      </div>
+<?php include('../txt/footer.txt'); ?>
+ 
