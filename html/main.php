7d8f2d6d2b399731e1efae649b07244641d0dea5
elsatarnbrant <119036906+elsatarnbrant@users.noreply.github.com>
2022-12-06 12:47:51 +0100

    k

--- html/main.php
+++ html/main.php
@@ -2,12 +2,14 @@
     <h1>LEGO BANK</h1>
     <p class="undertext">Hej</p>
     <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-    <div id="overlay-search">
-            <form class="searchform" action="searchpage.php" method="POST">
-                <input type="search" name="search" id="search" placeholder="Search your lego piece">
-                <button class="button" type="submit">Search</button>
-                <div class="button" id="help_btn">? </div>
-            </form>
+    <div class="hej">
+        <div id="overlay-search">
+                <form class="searchform" action="searchpage.php" method="POST">
+                    <input type="search" name="search" id="search" placeholder="Search your lego piece">
+                    <button class="button" type="submit">Search</button>
+                    <div class="button" id="help_btn">? </div>
+                </form>
+        </div>
     </div>
            
 
@@ -18,8 +20,7 @@
             <img src="../bilder/pil.png" alt="bil">
         </div>
     </div>
-    
-<?php
+   <?php
 
 
 $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
