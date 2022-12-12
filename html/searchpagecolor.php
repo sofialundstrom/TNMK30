eac5d6dada5e5873438cc4b0eba67f99fce71721
sofialundstrom <soflu921@student.liu.se>
2022-12-12 15:53:29 +0100

    f

--- html/searchpagecolor.php
+++ html/searchpagecolor.php
@@ -1,6 +1,6 @@
 <?php include('../txt/header.txt'); ?>
     <div class="searchContainer">
-        <form action="searchpage.php" method="POST">
+        <form action="searchpagepart.php" method="POST">
             <input class="searchBox" type="search" name="search" placeholder="Search...">
         </form>
     </div>
@@ -20,26 +20,34 @@ if(isset($_GET["part"])) {
     $part = $_GET["part"];
 }
 
-$sql_qurry = "SELECT DISTINCT inventory.SetID, inventory.Quantity, colors.Colorname, parts.Partname, inventory.ColorID, inventory.ItemtypeID, inventory.ItemID 
-FROM inventory, colors, parts WHERE inventory.ItemID='$part' AND inventory.ItemtypeID='P' AND parts.PartID=inventory.ItemID 
+
+$sql_qurry = "SELECT inventory.Quantity, colors.Colorname, parts.Partname, inventory.ColorID, inventory.ItemtypeID, inventory.ItemID
+FROM inventory, colors, parts WHERE inventory.ItemID='$part' AND inventory.ItemtypeID='P' AND parts.PartID=inventory.ItemID
 ORDER BY Colorname ASC, ColorID ASC LIMIT 50";
+
 
 $contents = mysqli_query ($connection, $sql_qurry );
 
+$row = mysqli_fetch_array($contents);
 
+$quantity = $row['Quantity'];
+$itemtype = $row['ItemtypeID'];
+$item = $row['ItemID'];
+$parts = $row['PartID'];
+$partname = $row['Partname'];
 
 print "<div class='container'>";
-    
-        while($row = mysqli_fetch_array($contents)){
-   $i = 0;
-   if($imagecolor == $i) {
 
-    $quantity = $row['Quantity'];
-    $imagecolor = $row['ColorID'];
-    $itemtype = $row['ItemtypeID'];
-    $item = $row['ItemID'];
-    $parts = $row['PartID'];
-    $partname = $row['Partname'];
+$sqlColor = "SELECT DISTINCT ColorID FROM inventory WHERE ItemID = '$part'";
+
+$colorContents = mysqli_query($connection, $sqlColor);
+
+
+
+while($colorRow = mysqli_fetch_array($colorContents)) {
+   
+    $imagecolor = $colorRow['ColorID'];
+
     $sqlImg = "SELECT * FROM images WHERE ItemtypeID = 'P' AND ItemID = '$item' AND ColorID = '$imagecolor'";
 
     $imagesearch = mysqli_query($connection, $sqlImg);
@@ -67,11 +75,8 @@ print "<div class='container'>";
                 <img src='$prefix$filename' alt='legopart' id='legopartpic'>$partname<br>$item  
             </div>
         </a>");
+    }
 
-    }
-    $i++;
-    
-    }
 print "</div>";
 mysqli_close($connection);
 
--- html/searchpagepart.php
+++ html/searchpagepart.php
@@ -6,6 +6,10 @@
     </div>
 
 <?php
+
+
+  
+
 $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
 if(!$connection){
     die('MySQL connection error');
