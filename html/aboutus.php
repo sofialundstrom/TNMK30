1248e81357c011534aeb2e76a28b047c0e6575a9
elsatarnbrant <119036906+elsatarnbrant@users.noreply.github.com>
2023-01-09 16:18:01 +0100

    :)

--- html/aboutus.php
+++ html/aboutus.php
@@ -5,45 +5,45 @@
     </div>
         <div class="container-aboutus">
             <div id="box1">
-            <img src="../bilder/donkey.jpg" class="lego" alt = "scary lego">
+            <img src="../bilder/donkey.jpg" class="lego-man" alt = "scary lego">
                 <div>
-                <h3>Sofia Lundström</h3>
+                <h3 class="name">Sofia Lundström</h3>
                 <p class="info">Favorite set: Hillside House</p>
                 <break>
                 <p class="info">E-post: soflu921@student.liu.se</p>
                 </div> 
                 </div>
                     <div id="box2">
-                    <img src="../bilder/donkey.jpg" class="lego" alt = "scary lego">
+                    <img src="../bilder/donkey.jpg" class="lego-man" alt = "scary lego">
                         <div>
-                            <h3>Abdiladiif Musse</h3>
+                            <h3 class="name">Abdiladiif Musse</h3>
                             <p class="info">Favorite set: Bird of Paradise</p> 
                             <break>
                             <p class="info"> E-post: adb543@student.liu.se</p>
                         </div>
                    </div>
                     <div id="box3">
-                    <img src="../bilder/donkey.jpg" class="lego" alt = "scary lego">
+                    <img src="../bilder/donkey.jpg" class="lego-man" alt = "scary lego">
                         <div>
-                        <h3>Vanessa Ivarsson</h3>
+                        <h3 class="name">Vanessa Ivarsson</h3>
                         <p class="info">Favorite set: Flower Bouquet</p>
                         <break>
                         <p class="info">E-post: vaniv810@student.liu.se</p>       
                     </div>
                 </div>
                     <div id="box4">
-                    <img src="../bilder/donkey.jpg" class="lego" alt = "scary lego">
+                    <img src="../bilder/donkey.jpg" class="lego-man" alt = "scary lego">
                         <div>
-                        <h3>Hanna Sjöström</h3>    
+                        <h3 class="name">Hanna Sjöström</h3>    
                         <p class="info">Favorite set: The First Night </p>
                         <break>
                         <p class="info">E-post: hansj128@student.liu.se</p>  
                     </div>   
                 </div>
                     <div id="box5">
-                    <img src="../bilder/donkey.jpg"class="lego" alt = "scary lego">
+                    <img src="../bilder/donkey.jpg"class="lego-man" alt = "scary lego">
                         <div>
-                        <h3>Elsa Tärnbrant</h3>
+                        <h3 class="name">Elsa Tärnbrant</h3>
                         <p class="info">Favorite set: Mulan's training area </p>
                         <break>
                         <p class="info">E-post: elsta728@student.liu.se</p>
