508e08204a5f5f449fa86ce9770c340af9cfd1c5
Hannisssss <Chat3y@gmail.com>
2023-01-11 10:16:04 +0100

    d

--- html/aboutus.php
+++ html/aboutus.php
@@ -1,52 +1,52 @@
 <?php include('../txt/header.txt'); ?>
 <link href="../css/style.css" media="screen" rel="stylesheet" type="text/css">
-    <div class="container-header">
-        <h2 class="aboutus-header">ABOUT US</h2>
+    <div class="containerHeader">
+        <h2 class="aboutUsHeader">ABOUT US</h2>
     </div>
-        <div class="container-aboutus">
-            <div id="box1">
-            <img src="../bilder/Sofia.png" class="lego-man" alt = "scary lego">
+        <div class="containerAboutUs">
+            <div class="aboutUsBox">
+            <img src="../bilder/Sofia.png" class="legoMan" alt = "lego self">
                 <div>
                 <h3 class="name">Sofia Lundström</h3>
-                <p class="info">Favorite set: Hillside House</p>
+                <p class="personalInfo">Favorite set: Hillsclasse House</p>
                 <break>
-                <p class="info">E-post: soflu921@student.liu.se</p>
+                <p class="personalInfo">E-post: soflu921@student.liu.se</p>
                 </div> 
                 </div>
-                    <div id="box2">
-                    <img src="../bilder/Abdiladiif.png" class="lego-man" alt = "scary lego">
+                    <div class="aboutUsBox">
+                    <img src="../bilder/Abdiladiif.png" class="legoMan" alt = "lego self">
                         <div>
                             <h3 class="name">Abdiladiif Musse</h3>
-                            <p class="info">Favorite set: Bird of Paradise</p> 
+                            <p class="personalInfo">Favorite set: Bird of Paradise</p> 
                             <break>
-                            <p class="info"> E-post: adb543@student.liu.se</p>
+                            <p class="personalInfo"> E-post: adb543@student.liu.se</p>
                         </div>
                    </div>
-                    <div id="box3">
-                    <img src="../bilder/Vanessa.png" class="lego-man" alt = "scary lego">
+                    <div class="aboutUsBox">
+                    <img src="../bilder/Vanessa.png" class="legoMan" alt = "lego self">
                         <div>
                         <h3 class="name">Vanessa Ivarsson</h3>
-                        <p class="info">Favorite set: Flower Bouquet</p>
+                        <p class="personalInfo">Favorite set: Flower Bouquet</p>
                         <break>
-                        <p class="info">E-post: vaniv810@student.liu.se</p>       
+                        <p class="personalInfo">E-post: vaniv810@student.liu.se</p>       
                     </div>
                 </div>
-                    <div id="box4">
-                    <img src="../bilder/Hanna.png" class="lego-man" alt = "scary lego">
+                    <div class="aboutUsBox">
+                    <img src="../bilder/Hanna.png" class="legoMan" alt = "lego self">
                         <div>
                         <h3 class="name">Hanna Sjöström</h3>    
-                        <p class="info">Favorite set: The First Night </p>
+                        <p class="personalInfo">Favorite set: The First Night </p>
                         <break>
-                        <p class="info">E-post: hansj128@student.liu.se</p>  
+                        <p class="personalInfo">E-post: hansj128@student.liu.se</p>  
                     </div>   
                 </div>
-                    <div id="box5">
-                    <img src="../bilder/Elsa.png"class="lego-man" alt = "scary lego">
+                    <div class="aboutUsBox">
+                    <img src="../bilder/Elsa.png"class="legoMan" alt = "lego self">
                         <div>
                         <h3 class="name">Elsa Tärnbrant</h3>
-                        <p class="info">Favorite set: Mulan's training area </p>
+                        <p class="personalInfo">Favorite set: Mulan's training area </p>
                         <break>
-                        <p class="info">E-post: elsta728@student.liu.se</p>
+                        <p class="personalInfo">E-post: elsta728@student.liu.se</p>
                         </div>   
                     </div>
     </div>
