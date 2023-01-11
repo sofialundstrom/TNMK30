6e5de19e887c5565321894ee5920ff3169fd2a78
sofialundstrom <soflu921@student.liu.se>
2023-01-11 12:27:45 +0100

    :)))

---
+++ html/help.php
@@ -0,0 +1,42 @@
+<!-- Include css, javascript,head and start of body -->
+<?php include('../txt/header.txt'); ?>
+
+<!-- Title -->
+<h1 id="helpHeader"> HOW TO SEARCH</h1>
+
+<!-- A container for all information boxes -->
+<div>
+    <div class="helpBox">
+        <img src="../bilder/light.helppic1.jpg" class="helpPic" alt="bild">
+        <div class="helpList">
+            <p>1. Enter your piece in the searchbox</p>
+        </div>
+    </div>
+    <div class="helpBox">
+        <img src="../bilder/light.helppic2.jpg" class="helpPic" alt="bild">
+        <div class="helpList">
+            <p>2. Click on your piece</p>  
+        </div>
+    </div>
+    <div class="helpBox">
+        <img src="../bilder/light.helppic3.jpg" class="helpPic" alt="bild">
+        <div class="helpList">
+            <p>3. Click on the color of your choice.</p>  
+        </div>
+    </div>
+    <div class="helpBox">
+        <img src="../bilder/light.helppic4.jpg" class="helpPic" alt="bild">
+        <div class="helpList">
+            <p>4. Chose a set</p>  
+        </div>
+    </div>
+    <div class="helpBox">
+        <img src="../bilder/light.helppic5.jpg" class="helpPic" alt="bild">
+        <div class="helpList">
+            <p>5. See info about set containing piece</p>  
+        </div>
+    </div>
+</div>
+
+<?php include('../txt/footer.txt'); ?>
+<!-- Include footer where body is closed -->
