babaead9e0635a74bbfe6338573e53fe8adfff28
Hannisssss <Chat3y@gmail.com>
2023-01-11 12:10:05 +0100

    d

--- html/aboutus.php
+++ html/aboutus.php
@@ -2,8 +2,8 @@
 <?php include('../txt/header.txt'); ?>
 
     <!-- Title -->
-    <div class="container-header">
-        <h2 class="aboutus-header">ABOUT US</h2>
+    <div class="containerHeader">
+        <h2 class="aboutUsHeader">ABOUT US</h2>
     </div>
 
     <!-- Container for information boxes to use flex box -->
@@ -12,48 +12,48 @@
     
         <!-- Info about the creaters of this website -->
 
-        <div id="box1">
-            <img src="../bilder/Sofia.png" class="lego-man" alt = "Lego Sofia">
+        <div class="aboutUsBox">
+            <img src="../bilder/Sofia.png" class="legoMan" alt = "Lego Sofia">
             <div>
                 <h3 class="name">Sofia Lundström</h3>
-                <p class="info">Favorite set: Hillside House</p><br>
-                <p class="info">E-post: soflu921@student.liu.se</p>
+                <p class="personalInfo">Favorite set: Hillside House</p>
+                <p class="personalInfo">E-post: soflu921@student.liu.se</p>
             </div> 
         </div>
 
-        <div id="box2">
-            <img src="../bilder/Abdiladiif.png" class="lego-man" alt = "Lego Abdiladiif">
+        <div class="aboutUsBox">
+            <img src="../bilder/Abdiladiif.png" class="legoMan" alt = "Lego Abdiladiif">
             <div>
                 <h3 class="name">Abdiladiif Musse</h3>
-                <p class="info">Favorite set: Bird of Paradise</p><br>
-                <p class="info"> E-post: adb543@student.liu.se</p>
+                <p class="personalInfo">Favorite set: Bird of Paradise</p>
+                <p class="personalInfo"> E-post: adb543@student.liu.se</p>
             </div>
         </div>
 
-        <div id="box3">
-            <img src="../bilder/Vanessa.png" class="lego-man" alt = "Lego Vanessa">
+        <div class="aboutUsBox">
+            <img src="../bilder/Vanessa.png" class="legoMan" alt = "Lego Vanessa">
             <div>
                 <h3 class="name">Vanessa Ivarsson</h3>
-                <p class="info">Favorite set: Flower Bouquet</p><br>
-                <p class="info">E-post: vaniv810@student.liu.se</p>       
+                <p class="personalInfo">Favorite set: Flower Bouquet</p>
+                <p class="personalInfo">E-post: vaniv810@student.liu.se</p>       
             </div>
         </div>
 
-        <div id="box4">
-            <img src="../bilder/Hanna.png" class="lego-man" alt = "Lego Hanna">
+        <div class="aboutUsBox">
+            <img src="../bilder/Hanna.png" class="legoMan" alt = "Lego Hanna">
             <div>
                 <h3 class="name">Hanna Sjöström</h3>    
-                <p class="info">Favorite set: The First Night </p><br>
-                <p class="info">E-post: hansj128@student.liu.se</p>  
+                <p class="personalInfo">Favorite set: The First Night </p>
+                <p class="personalInfo">E-post: hansj128@student.liu.se</p>  
             </div>   
         </div>
 
-        <div id="box5">
-            <img src="../bilder/Elsa.png"class="lego-man" alt = "Lego Elsa">
+        <div class="aboutUsBox">
+            <img src="../bilder/Elsa.png"class="legoMan" alt = "Lego Elsa">
             <div>
                 <h3 class="name">Elsa Tärnbrant</h3>
-                <p class="info">Favorite set: Mulan's training area </p><br>
-                <p class="info">E-post: elsta728@student.liu.se</p>
+                <p class="personalInfo">Favorite set: Mulan's training area </p>
+                <p class="personalInfo">E-post: elsta728@student.liu.se</p>
             </div>   
         </div>
     </div>
