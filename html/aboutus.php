ec05ee88bbe32427a2f50381ddafccd695c26d14
vanessaivarsson <vanessaivarsson@gmail.com>
2023-01-12 14:50:06 +0100

    about us pic

--- html/aboutus.php
+++ html/aboutus.php
@@ -18,7 +18,10 @@
             <img src="../bilder/Sofia.png" class="legoMan" alt = "Lego Sofia">
             <div>
                 <h3 class="name">Sofia Lundström</h3>
-                <p class="personalInfo">Favorite set: Hillside House</p>
+                <p class="personalSet">Favorite set: Hillside House</p>
+                    <div class="popup" style="display:none;" >
+                    <img src="../bilder/hill.set.jpg" alt="Popup Image">
+                    </div>
                 <p class="personalInfo">E-post: soflu921@student.liu.se</p>
             </div> 
         </div>
@@ -27,7 +30,10 @@
             <img src="../bilder/Abdiladiif.png" class="legoMan" alt = "Lego Abdiladiif">
             <div>
                 <h3 class="name">Abdiladiif Musse</h3>
-                <p class="personalInfo">Favorite set: Bird of Paradise</p>
+                <p class="personalSet">Favorite set: Bird of Paradise</p>
+                    <div class="popup" style="display:none;" >
+                    <img src="../bilder/set.bird.jpg.jpg" alt="Popup Image">
+                    </div>
                 <p class="personalInfo"> E-post: adbmu543@student.liu.se</p>
             </div>
         </div>
@@ -36,16 +42,22 @@
             <img src="../bilder/Vanessa.png" class="legoMan" alt = "Lego Vanessa">
             <div>
                 <h3 class="name">Vanessa Ivarsson</h3>
-                <p class="personalInfo">Favorite set: Flower Bouquet</p>
+                <p class="personalSet">Favorite set: Flower Bouquet</p>
+                <div class="popup" style="display:none;" >
+                    <img src="../bilder/set.flower.jpg" alt="Popup Image">
+                </div>
                 <p class="personalInfo">E-post: vaniv810@student.liu.se</p>       
-            </div>
+                </div>
         </div>
 
         <div class="aboutUsBox">
             <img src="../bilder/Hanna.png" class="legoMan" alt = "Lego Hanna">
             <div>
                 <h3 class="name">Hanna Sjöström</h3>    
-                <p class="personalInfo">Favorite set: The First Night </p>
+                <p class="personalSet">Favorite set: The First Night </p>
+                    <div class="popup" style="display:none;" >
+                    <img src="../bilder/night.set.jpg" alt="Popup Image">
+                    </div>
                 <p class="personalInfo">E-post: hansj128@student.liu.se</p>  
             </div>   
         </div>
@@ -54,7 +66,10 @@
             <img src="../bilder/Elsa.png" class="legoMan" alt = "Lego Elsa">
             <div>
                 <h3 class="name">Elsa Tärnbrant</h3>
-                <p class="personalInfo">Favorite set: Mulan's training area </p>
+                <p class="personalSet">Favorite set: Mulan's training area </p>
+                    <div class="popup" style="display:none;" >
+                    <img src="../bilder/set.mulan.jpg" alt="Popup Image">
+                    </div>
                 <p class="personalInfo">E-post: elsta728@student.liu.se</p>
             </div>   
         </div>
